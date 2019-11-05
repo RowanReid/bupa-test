7ed98bd4beadaab4076f996b2c197c9a4eeb7244
RowanReid <rowan@sideclick.co.za>
2019-11-05 16:57:35 +0200

    Update chat-bot.js

--- chat-bot.js
+++ chat-bot.js
@@ -1,33 +1,54 @@
 var codeTest = false;
 
 function onConversationsAPIReady() {
-
+  
   if(codeTest == true) {
+
     return;
+
   }else {
+
     codeTest = true;
 
     window.hsConversationsSettings = {
+
       loadImmediately: false,
+
     };
 
+ 
+
     window.HubSpotConversations.clear();
+    window.HubSpotConversations.widget.refresh();
+    window.HubSpotConversations.widget.remove();
     window.HubSpotConversations.widget.load();
     window.HubSpotConversations.on('conversationClosed', payload => {
       window.HubSpotConversations.clear();
+      window.HubSpotConversations.widget.refresh();
+      window.HubSpotConversations.widget.remove();
+      window.HubSpotConversations.widget.load();
+      console.log('Conversation Closed');
     });
-  }
 
+  }
 }
 
 if (window.HubSpotConversations) {
+
   onConversationsAPIReady();
+
 }else {
   window.hsConversationsOnReady = [onConversationsAPIReady];
+
 }
 
 setTimeout(function(){ 
+
   if(codeTest != true) {
+
     onConversationsAPIReady();
+    console.log('Timer done');
+
   }
+
 }, 10000);
